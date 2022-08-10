b3aed7fc0841c3cc6e24084fff168ca5d17f9e2d
imshubhampatel <shubhampatel2024@gmail.com>
2022-08-10 16:12:46 +0530

    Feat : Added Knowlarity statics api

--- functions/src/knowlarity.ts
+++ functions/src/knowlarity.ts
@@ -1,6 +1,6 @@
-import axios, { AxiosRequestConfig } from "axios";
+// import axios, { AxiosRequestConfig } from "axios";
 
-const KPI_CALL_URL = "https://kpi.knowlarity.com{channel}/v1/call/";
+// const KPI_CALL_URL = "https://kpi.knowlarity.com{channel}/v1/call/";
 
 const handleKnowlarityCall = async (req: any) => {
     // setTimeout(() => {
@@ -8,26 +8,26 @@ const handleKnowlarityCall = async (req: any) => {
     // }, 100);
 };
 
-const balanceHelper = async (req: any) => {
-    const params: AxiosRequestConfig<any> = {
-        data: {
-            body: {
-                start_time: req["start_time"],
-                end_time: Date.now(),
-                agent_number: req["agent_number"],
-                customer_number: req["customer_number"],
-            },
-            head: {},
-        },
-    };
+// const balanceHelper = async (req: any) => {
+//     const params: AxiosRequestConfig<any> = {
+//         data: {
+//             body: {
+//                 start_time: req["start_time"],
+//                 end_time: Date.now(),
+//                 agent_number: req["agent_number"],
+//                 customer_number: req["customer_number"],
+//             },
+//             head: {},
+//         },
+//     };
 
-    try {
-        let log_data = await axios.get(KPI_CALL_URL, params);
+//     try {
+//         let log_data = await axios.get(KPI_CALL_URL, params);
 
-        console.log(log_data);
-    } catch (e) {
-        console.log(e);
-    }
-};
+//         console.log(log_data);
+//     } catch (e) {
+//         console.log(e);
+//     }
+// };
 
 export default handleKnowlarityCall;
